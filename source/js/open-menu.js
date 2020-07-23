86a3654f3b13c65ff5ce76b1a304609af26f6cb2
Pavel Ulianov <paveluv@mail.ru>
2020-07-23 13:50:04 +0300

    Выполнения задания модуль 3, итого за 23.07.2020, закончил index.html

--- source/js/open-menu.js
+++ source/js/open-menu.js
@@ -1,10 +1,10 @@
-let menuElement = document.querySelector('.main-nav__wrapper');
-let openMenuBtn = document.querySelector('.main-nav__toggle');
+let headerElement = document.querySelector('.js-header');
+let openMenuBtn = document.querySelector('.js-menu-toggle');
 let labelMenuElement = document.querySelector('.js-label')
 
   openMenuBtn.addEventListener('click', function (element) {
     element.preventDefault();
-    menuElement.classList.toggle('open-menu');
+    headerElement.classList.toggle('open-menu');
     openMenuBtn.classList.toggle('close-menu');
     if(openMenuBtn.classList.contains('close-menu')) {
       labelMenuElement.textContent = 'Закрыть меню'
