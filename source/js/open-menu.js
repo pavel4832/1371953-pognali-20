a30230c486410c3548b270a427639377f4666b8e
Pavel Ulianov <paveluv@mail.ru>
2020-07-24 05:55:09 +0300

    Выполнения задания модуль 3, закончил form.html

--- source/js/open-menu.js
+++ source/js/open-menu.js
@@ -1,4 +1,5 @@
 let headerElement = document.querySelector('.js-header');
+let logoElement = document.querySelector('.js-image');
 let openMenuBtn = document.querySelector('.js-menu-toggle');
 let labelMenuElement = document.querySelector('.js-label')
 
@@ -7,8 +8,10 @@ let labelMenuElement = document.querySelector('.js-label')
     headerElement.classList.toggle('open-menu');
     openMenuBtn.classList.toggle('close-menu');
     if(openMenuBtn.classList.contains('close-menu')) {
-      labelMenuElement.textContent = 'Закрыть меню'
+      labelMenuElement.textContent = 'Закрыть меню';
+      logoElement.src = 'img/logo-mobile-blue@1x.png';
     } else {
-      labelMenuElement.textContent = 'Открыть меню'
+      labelMenuElement.textContent = 'Открыть меню';
+      logoElement.src = 'img/logo-mobile-white@1x.png'
     }
   })
