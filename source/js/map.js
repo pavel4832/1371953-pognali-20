b5c3b2d16068bfe148feb1840b0a57080e6f3cea
Pavel Ulianov <paveluv@mail.ru>
2020-07-26 14:34:55 +0300

    Модуль 3, задание 2. Завершил index.html

---
+++ source/js/map.js
@@ -0,0 +1,28 @@
+let backgroundMap = document.querySelector('.js-background')
+
+backgroundMap.style.display = 'none';
+
+ymaps.ready(init);
+
+function init(){
+
+  var myMap = new ymaps.Map("map", {
+    center: [59.938635, 30.323118],
+    zoom: 16,
+    controls: []
+  }),
+
+  MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+    '<div>$[properties.iconContent]</div>'
+  ),
+
+  myPlacemark = new ymaps.Placemark([59.938635, 30.323118], {}, {
+    iconLayout: 'default#imageWithContent',
+    iconImageHref: '../img/map-marker.svg',
+    iconImageSize: [41, 41],
+    iconContentLayout: MyIconContentLayout
+  });
+
+  myMap.geoObjects.add(myPlacemark);
+  myMap.behaviors.disable('scrollZoom');
+}
