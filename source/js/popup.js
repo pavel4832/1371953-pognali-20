6546682d886b1a97ee859e126f675e1909f959a6
Pavel Ulianov <paveluv@mail.ru>
2020-08-24 13:53:37 +0300

    Модуль 5, задание 2.

---
+++ source/js/popup.js
@@ -0,0 +1,22 @@
+let popupElement = document.querySelector('.js-popup');
+let openBtn = document.querySelector('.js-open-popup');
+let closeBtn = document.querySelector('.js-close-popup');
+
+openBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+  popupElement.classList.add('popup-show');
+});
+
+closeBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+  popupElement.classList.remove('popup-show');
+});
+
+window.addEventListener('keydown', function (e) {
+  if (e.keyCode === 27) {
+    if (popupElement.classList.contains('popup-show')) {
+      e.preventDefault();
+      popupElement.classList.remove('popup-show');
+    }
+  }
+});
