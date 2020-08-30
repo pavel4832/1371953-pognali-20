6b390acf9bfc5a9c810da51333e7d842d5f3c09d
Pavel Ulianov <paveluv@mail.ru>
2020-08-30 07:51:04 +0300

    Модуль 7, задание 2. спрайт во внешний файл, стили по состоянию для form.html

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ const less = require("gulp-less");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
+const rename = require("gulp-rename");
+const svgstore = require("gulp-svgstore");
 
 // Styles
 
@@ -49,3 +51,14 @@ const watcher = () => {
 exports.default = gulp.series(
   styles, server, watcher
 );
+
+// SVG sprite
+
+const sprite = () => {
+  return gulp.src("source/img/**/icon-*.svg")
+    .pipe(svgstore())
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("source/img"))
+}
+
+exports.sprite = sprite;
